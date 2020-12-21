001ddb8ef415665ecb514623449112eebb0042ab
jtornert <joakim.tornert@outlook.com>
2020-12-21 21:18:21 +0100

    organized images and setup save and load state

--- main.py
+++ main.py
@@ -2,37 +2,56 @@ import cv2 as cv
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plot
+import glob
+import os
 
-mnist = tf.keras.datasets.mnist
+from tensorflow.keras import callbacks
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(train_images, train_labels), (test_images,
+                               test_labels) = tf.keras.datasets.mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+train_images = tf.keras.utils.normalize(train_images, axis=1)
+test_images = tf.keras.utils.normalize(test_images, axis=1)
+
+checkpoint_path = 'training_1/cp.ckpt'
+checkpoint_dir = os.path.dirname(checkpoint_path)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path, save_weights_only=True, verbose=1)
 
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.sigmoid))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.sigmoid))
 model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.load_weights(checkpoint_path)
+model.fit(train_images,
+          train_labels,
+          epochs=40,
+          validation_data=(test_images, test_labels),
+          callbacks=[cp_callback])
 
-model.fit(x_train, y_train, epochs=3)
-
-# model.save('digits.model')
-
-test = model.predict(x_test)
+test = model.predict(test_images)
 
 for i in range(9):
-  img = np.array(x_test[i], dtype='float').reshape((28,28))
-  print(f'Label: {y_test[i]}\tPredicted: {np.argmax(test[i])}')
-  plot.imshow(img, cmap='gray_r')
-  plot.show()
+    img = np.array(test_images[i], dtype='float').reshape((28, 28))
+    print(f'Label: {test_labels[i]}\tPredicted: {np.argmax(test[i])}')
+    plot.imshow(img, cmap='gray_r')
+    plot.show()
 
-img = cv.imread('7_1.png')[:,:,0]
-img = np.invert([img])
-prediction = model.predict(img)
-print(np.argmax(prediction))
-plot.imshow(img[0], cmap='gray_r')
-plot.show()
+labels = [1, 2, 4, 5, 5, 6, 7, 7, 8]
+i = 0
+print('\nTest images:')
+
+for filepath in glob.iglob(r'./img/*.png'):
+    img = cv.imread(filepath)[:, :, 0]
+    img = np.invert([img])
+    prediction = model.predict(img)
+    print(f'Label: {labels[i]}\tPredicted: {np.argmax(prediction)}')
+    i = i + 1
+    plot.imshow(img[0], cmap='gray_r')
+    plot.show()
